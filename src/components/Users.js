d21211622ac4277fa15cd7f2713d7330e6130634
github username <github email address>
2019-09-17 16:59:24 -0500

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Users extends Component {
 
   render() {
     return (
       <div>
+         {this.props.userCount}
         <ul>
-          Users!
+          {this.props.users.map((user, index) => {
+            return <li key={index}>{user.username}</li>
+          })}
         </ul>
       </div>
     )
   }
 }
 
+const mapStateToProps = state => {
+  // debugger
+  return {
+    users: state.users,
+    userCount: state.users.length
+  }
+}
+
 //add mapStateToProps here
 
-export default Users
+export default connect(mapStateToProps)(Users)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import manageUsers from './reducers/manageUsers'
+import manageUsers from "./reducers/manageUsers";
 
+// creating store that takes in manageUsers reducer
+const store = createStore(manageUsers);
 
-// add imports and code
-
-
+// Adding Provider will grant all components access to store
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
